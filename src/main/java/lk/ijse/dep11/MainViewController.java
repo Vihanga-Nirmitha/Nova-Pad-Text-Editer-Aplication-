7ffcdcc97ec6a9b1dd1e302f0385bdf1ae56ed5f
Vihanga Nirmitha <vihanganirmitha200@gmail.com>
2023-08-22 01:22:34 +0530

    About window popup

--- src/main/java/lk/ijse/dep11/MainViewController.java
+++ src/main/java/lk/ijse/dep11/MainViewController.java
@@ -1,5 +1,6 @@
 package lk.ijse.dep11;
 
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,7 +9,9 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainViewController {
     public MenuBar mnmain;
@@ -40,13 +43,30 @@ public class MainViewController {
     }
 
     public void mnexitOnAction(ActionEvent actionEvent) {
-      
+
 
     }
 
-    public void mnuserguidOnAction(ActionEvent actionEvent) {
+    public void mnuserguidOnAction(ActionEvent actionEvent) throws  Exception{
+        AnchorPane guidroot = FXMLLoader.load(getClass().getResource("/view/UserGuid.fxml"));
+        Scene guidscene = new Scene(guidroot);
+        Stage guidstage = new Stage();
+        guidstage.initModality(Modality.APPLICATION_MODAL);
+        guidstage.setScene(guidscene);
+        guidstage.setTitle("New Modal Window");
+        guidstage.setAlwaysOnTop(true);
+        guidstage.show();
     }
 
-    public void mnaboutOnAction(ActionEvent actionEvent) {
+
+    public void mnaboutOnAction(ActionEvent actionEvent) throws  Exception{
+
+        AnchorPane aboutroot = FXMLLoader.load(getClass().getResource("/view/AboutUs.fxml"));
+        Scene aboutscene = new Scene(aboutroot);
+        Stage aboutstage = new Stage();
+        aboutstage.setScene(aboutscene);
+        aboutstage.initStyle(StageStyle.TRANSPARENT);
+        aboutstage.centerOnScreen();
+        aboutstage.show();
     }
 }
